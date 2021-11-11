accd63821ace79139d13f7890945ff5761a06f64
Ali Abdullah <zshare_187@hotmail.com>
2021-11-11 11:36:07 +0000

    Update PortScanner.py

--- PortScanner.py
+++ PortScanner.py
@@ -3,7 +3,8 @@ import sys
 
 ## Provide an Ip Address in the arguments call .. and that's it
 # This Tool Scans For Http Servers with specific ip 
-  
+##### basic usage #####
+# python3 PortScanner.py <victim Ip>
 ip_address = sys.argv[1]
 for i in range(65000):
   result = os.system("curl %s:%d 2>/dev/null | grep -w '<html>'" %(ip_address ,i))
